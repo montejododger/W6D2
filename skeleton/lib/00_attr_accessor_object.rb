e61ae4a3a980af0c64061f2f3b33a33a65137c7f
Matthew Montejo <montejododger@yahoo.com>
2023-05-02 16:27:44 -0700

    solutions added

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,21 @@
 class AttrAccessorObject
-  def self.my_attr_accessor(*names)
-    # ...
-  end
+	def self.my_attr_accessor(*names)
+		#iterate thru the names args
+	names.each do |name|
+		#we then define a method(aka new method) with the name
+		define_method(name) do
+			# we get the variable withthe instance_variable_get
+			instance_variable_get("@#{name}")
+		end
+
+		#define a method that sets a variable
+		#interpolation is key 
+		define_method("#{name}=") do |value|
+			# use instance variable set
+			instance_variable_set("@#{name}", value)
+		end
+	end
+
+	end
 end
+
